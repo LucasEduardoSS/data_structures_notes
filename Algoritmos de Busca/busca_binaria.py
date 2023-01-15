23695ca995844e08aedb4c019fa90b760fa97ae8
Lucas Eduardo dos Santos de Souza <84149268+LucasEduardoSS@users.noreply.github.com>
2023-01-15 22:33:33 +0000

    Mensagem final mostra em qual iteração a função encontrou o número procurado.
    Final message shows in which iteration the function found the number
    searched.

--- Algoritmos de Busca/busca_binaria.py
+++ Algoritmos de Busca/busca_binaria.py
@@ -4,6 +4,7 @@ busca = int(input('Digite um valor: '))
 i = 0
 inicio = 0
 fim = len(lista)
+cont = 0
 
 metade = int((fim - inicio)/2)
 while i <= len(lista) and achou is False:
@@ -13,13 +14,14 @@ while i <= len(lista) and achou is False:
         if busca < lista[metade]:
             fim = metade
             metade = int((fim - inicio)/2)
-            print('Mundaça 1')
+
         else:
             inicio = metade
             metade = int((inicio + fim)/2)
+    cont += 1
 
 if achou is True:
-    print(f'O valor foi encontrado na {metade + 1}° posição.')
+    print(f'O valor foi encontrado na {metade + 1}° posição na {cont}° iteração.')
 else:
     print('O valor não foi encontrado.')
 
